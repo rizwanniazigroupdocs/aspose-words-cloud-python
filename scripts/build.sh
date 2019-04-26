66a04d2738875a4d6bbb1c39f6201569b4055512
Evgeny <ekuleshov@live.com>
2019-04-26 09:24:09 +0300

    Test command added to build file

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,1 +1,1 @@
-python -m pip install -r requirements.txt && python -m pip install -r test-requirements.txt
+python -m pip install -r requirements.txt && python -m pip install -r test-requirements.txt && nosetests --with-xunit
