32345db53cb280916de7151d53606ab677b5c567
Evgeny <ekuleshov@live.com>
2019-05-08 11:14:42 +0300

    publish stage

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -7,3 +7,5 @@ touch ~/.pypirc
 echo [distutils] \\n index-servers= \\n\\t pypi \\n\\t pypitest\\n [pypi] \\n username = $1 \\n password = $2 \\n [pypitest]\\n repository = https://test.pypi.org/legacy/ \\n username = $1 \\n password = $2 > ~/.pypirc
 cat ~/.pypirc
 python setup.py sdist && twine upload dist/* -r pypitest
+rm -rf aspose_words_cloud.egg-info
+rm -rf dist
