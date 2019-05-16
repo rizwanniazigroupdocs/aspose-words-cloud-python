2d8fb3d57eb15f00ff4a8a671891c9018d5c057c
Evgeny <ekuleshov@live.com>
2019-05-16 13:46:43 +0300

    retry

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,9 @@
 git reset --hard
+git checkout
 pip install --upgrade pip
 python -m pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
-python -m pip install --upgrade twine
+python -m pip install --upgrade twine python-dateutil
 touch ~/.pypirc
 printf "[distutils] \\nindex-servers= \\n\\tpypi \\n\\tpypitest\\n[pypi] \\nusername: $1 \\npassword: $2 \\n[pypitest]\\nrepository: https://test.pypi.org/legacy/ \\nusername: $1 \\npassword: $2" > .pypirc
 cat ~/.pypirc
