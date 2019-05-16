3f4595b7e99d9e1f2b01e7308ab3f70f8059c083
Evgeny <ekuleshov@live.com>
2019-05-16 10:47:12 +0300

    Fixed publishing script

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ python -m pip install --upgrade setuptools wheel
 python setup.py sdist bdist_wheel
 python -m pip install --upgrade twine
 touch ~/.pypirc
-echo [distutils] \\n index-servers= \\n\\t pypi \\n\\t pypitest\\n[pypi] \\n username: $1 \\n password: $2 \\n[pypitest]\\n repository: https://test.pypi.org/legacy/ \\n username: $1 \\n password: $2 > ~/.pypirc
+printf "[distutils] \\nindex-servers= \\n\\tpypi \\n\\tpypitest\\n[pypi] \\nusername: $1 \\npassword: $2 \\n[pypitest]\\nrepository: https://test.pypi.org/legacy/ \\nusername: $1 \\npassword: $2" > .pypirc
 python setup.py sdist
 twine upload dist/* -r pypitest
 chmod 777 -R aspose_words_cloud.egg-info
